1d001e8483cefefa8590e632b73e3e304e37ea26
JamieLH07 <109854469+JamieLH07@users.noreply.github.com>
2024-03-27 15:10:51 +0000

    Add files via upload

---
+++ NUnitPlaywright.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NuGet.Frameworks;
+using NUnit.Framework;
+
+namespace PlaywrightTrainingProject;
+
+public class NUnitPlaywright : PageTest
+{
+    [SetUp]
+    public async Task Setup()
+
+    {
+
+        await Page.GotoAsync("https://www.saucedemo.com/");
+
+
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+
+        await Page.Locator("#user-name").FillAsync("standard_user");
+        await Page.Locator("#password").FillAsync("secret_sauce");
+        await Page.ClickAsync("text=Login");
+        await Expect(Page.Locator("text='Products'")).ToBeVisibleAsync();
+
+
+        //Use the Below to take a Snapshot of the page if there are any issues
+        //await page.ScreenshotAsync(new PageScreenshotOptions
+        //{
+        //    Path = "SauceDemo.jpg"
+
+        //});
+
+
+
+    }
+
+}
---
+++ NUnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Playwright;
+using NuGet.Frameworks;
+using NUnit.Framework;
+
+namespace PlaywrightTrainingProject;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+
+        //Playwright
+        using var playwright = await Playwright.CreateAsync();
+
+        //Browser
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+
+        {
+            Headless = false
+
+        });
+
+        //Page
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://www.saucedemo.com/");
+        await page.Locator("#user-name").FillAsync("standard_user");
+        await page.Locator("#password").FillAsync("secret_sauce");
+        await page.ClickAsync("text=Login");
+        var isExist = await page.Locator("text='Products'").IsVisibleAsync();
+        Assert.IsTrue(isExist);
+
+
+        //Use the Below to take a Snapshot of the page if there are any issues
+        //await page.ScreenshotAsync(new PageScreenshotOptions
+        //{
+        //    Path = "SauceDemo.jpg"
+
+        //});
+
+
+
+    }
+
+}
