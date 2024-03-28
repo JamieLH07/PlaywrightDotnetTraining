3811d93775a926d1040e1bae55a70b12f41d4c3f
Jamie.L.Hart <Jamie.L.Hart07@gmail.com>
2024-03-28 15:36:24 +0000

    Added new test for adding products to a basket and then logging out.
    Added new Page file for the Products page.

---
+++ Pages/ProductsPage.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrightTrainingProject.Pages
+{
+    public class ProductsPage
+    {
+        private IPage _page;
+        public ProductsPage(IPage page) => _page = page;
+
+        //Locator for Backpack at the cart button
+        private ILocator _btnBackpackCart => _page.Locator("#add-to-cart-sauce-labs-backpack");
+        private ILocator _btnBikeCart => _page.Locator("#add-to-cart-sauce-labs-bike-light");
+        private ILocator _btnShirtCart => _page.Locator("#add-to-cart-sauce-labs-bolt-t-shirt");
+        private ILocator _btnJacketCart => _page.Locator("#add-to-cart-sauce-labs-fleece-jacket");
+        private ILocator _btnOnesieCart => _page.Locator("#add-to-cart-sauce-labs-onesie");
+        private ILocator _btnRedTCart => _page.Locator("css=#add-to-cart-test\\.allthethings\\(\\)-t-shirt-\\(red\\)");
+        private ILocator _btnBasket => _page.Locator("css=#shopping_cart_container > a > span");
+        private ILocator _btnLeftPanel => _page.Locator("#react-burger-menu-btn");
+        private ILocator _btnLogout => _page.Locator("#logout_sidebar_link");
+        private ILocator _btnLogin => _page.Locator("#login-button");
+
+
+        public async Task AddAllToBasket()
+        {
+            await _btnBackpackCart.ClickAsync();
+            await _btnBikeCart.ClickAsync();
+            await _btnShirtCart.ClickAsync();
+            await _btnJacketCart.ClickAsync();
+            await _btnOnesieCart.ClickAsync();
+            await _btnRedTCart.ClickAsync();
+        }
+
+        public async Task<bool> IsBasketUpdated() => await _btnBasket.IsVisibleAsync();
+
+        public async Task Logout()
+        {
+            await _btnLeftPanel.ClickAsync();
+            await _btnLogout.ClickAsync();
+
+        }
+
+        public async Task<bool> IsLogoutSuccessful() => await _btnLogin.IsVisibleAsync();
+
+    }
+}
---
+++ PlaywrightDemo/PlaywrightPostLoginTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NuGet.Frameworks;
+using NUnit.Framework;
+using PlaywrightTrainingProject.Pages;
+
+namespace PlaywrightTrainingProject.PlaywrightDemo;
+
+public class PlaywrightPostLoginTests : PageTest
+{
+    [SetUp]
+    public async Task Setup()
+
+    {
+
+        await Page.GotoAsync("https://www.saucedemo.com/", new PageGotoOptions
+        {
+            //This will be used instead of the auto wait in playwright (This may not be needed as Playwright but the options are there if neede)
+            WaitUntil = WaitUntilState.DOMContentLoaded
+        });
+
+    }
+
+    [Test]
+    public async Task LoginAndAddToBasket()
+    {
+        //Get the page variable using the POM 
+        var page = await Browser.NewPageAsync();
+        var loginPage = new LoginPageUpgraded(page);
+
+
+        //Navigate to the website
+        await page.GotoAsync("https://www.saucedemo.com/");
+
+
+        //Do the Login using the POM
+        await loginPage.Login(userName: "standard_user", password: "secret_sauce");
+
+        //Check login was succesful
+        var ProductExists = await loginPage.IsProductTxtExists();
+        Assert.IsTrue(ProductExists);
+
+        var productPage = new ProductsPage(page);
+
+        //Add all products to the Basket
+        await productPage.AddAllToBasket();
+
+        //Check products have been added
+        var BasketUpdated = await productPage.IsBasketUpdated();
+        Assert.IsTrue(BasketUpdated);
+
+
+        //Logout
+        await productPage.Logout();
+
+        //Check Logout Was Successful
+        var LogoutCheck = await productPage.IsLogoutSuccessful();
+        Assert.IsTrue(LogoutCheck);
+
+    }
+}
